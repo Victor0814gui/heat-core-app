e79e6fdc6323534a35cced03d9d24b6799d8fac0
Victor Guilherme <92493696+Victor0814gui@users.noreply.github.com>
2023-01-23 00:24:17 -0300

    theme dark added

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer,DefaultTheme } from '@react-navigation/native';
 import { SplashScreen } from "./src/shared/splash_screen";
 import { Router } from './src/shared/routes';
 
 
+const MyTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: "#2e2e2e"
+  },
+};
+
 
 export default function App(){
   return(
-    <NavigationContainer>
+    <NavigationContainer theme={MyTheme}>
       <Router/>
     </NavigationContainer>
   )
