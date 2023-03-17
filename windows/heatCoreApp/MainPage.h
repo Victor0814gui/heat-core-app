a4d33d64626b9cbedac97f337a8d49a058ba4088
Victor Guilherme <92493696+Victor0814gui@users.noreply.github.com>
2023-03-17 17:00:05 -0300

    🎈

---
+++ windows/heatCoreApp/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::heatCoreApp::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::heatCoreApp::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
